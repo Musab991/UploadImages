0db916559a673fee3196385465d53f28d1396703
Musab991 <168702258+Musab991@users.noreply.github.com>
2024-08-29 11:48:11 +0300

    Add files via upload

---
+++ script1.sql
@@ -0,0 +1,4 @@
+CREATE TABLE TempImageUrls (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    ImageUrl NVARCHAR(500)
+);
---
+++ script2.sql
@@ -0,0 +1,27 @@
+-- Create a Common Table Expression (CTE) to assign random numbers to each image URL
+WITH RandomImages AS (
+    SELECT
+        ImageUrl,
+        ROW_NUMBER() OVER (ORDER BY NEWID()) AS RowNum
+    FROM TempImageUrls
+),
+
+-- Create a CTE to match items with the random images
+ItemsWithImages AS (
+    SELECT
+        i.ItemId AS ItemId,
+        ri.ImageUrl
+    FROM TbItems i
+    INNER JOIN RandomImages ri
+    ON i.ItemId % (SELECT COUNT(*) FROM RandomImages) + 1 = ri.RowNum
+)
+
+-- Update TbItems with the random image URLs
+UPDATE i
+SET i.ImageName = ri.ImageUrl
+FROM TbItems i
+INNER JOIN ItemsWithImages ri
+ON i.ItemId = ri.ItemId;
+
+
+DROP TABLE TempImageUrls;
